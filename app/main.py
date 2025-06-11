050e68f2edc682307f736fee603463c3e494e886
Marzzio Chicana Romero <89101069+marzzioChicana@users.noreply.github.com>
2025-06-11 17:36:55 -0500

    add cors support

--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from app.api.auth_routes import router as auth_router
 from app.db.session import engine
 from app.db.base import Base
+from app.core.cors import configure_cors
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+configure_cors(app)
+
 app.include_router(auth_router, prefix="/api/auth", tags=["auth"])
